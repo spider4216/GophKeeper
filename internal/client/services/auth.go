7faa22811cf20e8ff7e79b2fd5d65703b99afe6d
Yuriy Sirotenko <yosirotenko@gmail.com>
2026-08-30 10:31:04 +0500

    cli generic

--- internal/client/services/auth.go
+++ internal/client/services/auth.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log/slog"
 	"net/http"
 	"net/url"
 
@@ -18,63 +19,29 @@ const (
 )
 
 func (s *Service) Register(ctx context.Context, req shrModel.RegisterReq) (*shrModel.RegisterResp, error) {
-	data, err := json.Marshal(req)
-	if err != nil {
-		return nil, err
-	}
-
 	url, err := url.JoinPath(s.host, regURL)
 	if err != nil {
 		return nil, err
 	}
 
-	r, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(data))
-	if err != nil {
-		return nil, fmt.Errorf("cannot create request: %w", err)
-	}
-
-	r.Header.Add("Content-Type", "application/json")
-
-	resp, err := s.client.Do(r)
-	if err != nil {
-		return nil, err
-	}
-
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			s.logger.Warn("Cannot close body in register method")
-		}
-	}()
-
-	if resp.StatusCode != http.StatusCreated {
-		return nil, fmt.Errorf("response status is %d", resp.StatusCode)
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var res shrModel.RegisterResp
-
-	if err := json.Unmarshal(body, &res); err != nil {
-		return nil, err
-	}
-
-	return &res, nil
+	return postRequest[shrModel.RegisterReq, shrModel.RegisterResp](ctx, url, req, s.client, *s.logger, http.StatusCreated)
 }
 
 func (s *Service) Login(ctx context.Context, req shrModel.LoginReq) (*shrModel.LoginResp, error) {
-	data, err := json.Marshal(req)
-	if err != nil {
-		return nil, err
-	}
-
 	url, err := url.JoinPath(s.host, authURL)
 	if err != nil {
 		return nil, err
 	}
 
+	return postRequest[shrModel.LoginReq, shrModel.LoginResp](ctx, url, req, s.client, *s.logger, http.StatusOK)
+}
+
+func postRequest[TReq, TResp any](ctx context.Context, url string, req TReq, cli *http.Client, logger slog.Logger, expCode int) (*TResp, error) {
+	data, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
 	r, err := http.NewRequestWithContext(ctx, http.MethodPost, url, bytes.NewReader(data))
 	if err != nil {
 		return nil, fmt.Errorf("cannot create request: %w", err)
@@ -82,18 +49,18 @@ func (s *Service) Login(ctx context.Context, req shrModel.LoginReq) (*shrModel.L
 
 	r.Header.Add("Content-Type", "application/json")
 
-	resp, err := s.client.Do(r)
+	resp, err := cli.Do(r)
 	if err != nil {
 		return nil, err
 	}
 
 	defer func() {
 		if err := resp.Body.Close(); err != nil {
-			s.logger.Warn("Cannot close body in login method")
+			logger.Warn("Cannot close body in login method")
 		}
 	}()
 
-	if resp.StatusCode != http.StatusOK {
+	if resp.StatusCode != expCode {
 		return nil, fmt.Errorf("response status is %d", resp.StatusCode)
 	}
 
@@ -102,7 +69,7 @@ func (s *Service) Login(ctx context.Context, req shrModel.LoginReq) (*shrModel.L
 		return nil, err
 	}
 
-	var res shrModel.LoginResp
+	var res TResp
 
 	if err := json.Unmarshal(body, &res); err != nil {
 		return nil, err
